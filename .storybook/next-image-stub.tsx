d351e13cad266785ad7078f8a4f2c5c151cd14b9
cobibean <cobibean777@gmail.com>
2025-10-24 18:11:20 -0500

    chore: remove Storybook configuration and related stubs
    - Deleted Storybook configuration files, including `main.ts`, `preview.ts`, and stubs for `next/image` and `next/link`.
    - Removed unused HTML preview head and TypeScript configuration for Storybook.
    - Cleared out the `rich-text.tsx` file, which was not in use, to streamline the codebase.

--- .storybook/next-image-stub.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-type Props = React.ImgHTMLAttributes<HTMLImageElement> & { fill?: boolean };
-const Image = ({ src = "", alt = "", ...rest }: Props) => <img src={String(src)} alt={alt} {...rest} />;
-export default Image as any;
-
--- .storybook/next-link-stub.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-type Props = React.AnchorHTMLAttributes<HTMLAnchorElement> & { href: string };
-const Link = ({ href, children, ...rest }: Props) => (
-  <a href={href} {...rest}>
-    {children}
-  </a>
-);
-export default Link as any;
-
